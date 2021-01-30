6b964f0616edcbc19683732033f35c5d66a195ac
arron.moss@zero1.co.uk <arron>
2021-01-30 15:58:16 +0000

    unsetting minisearch

--- app/etc/config.php
+++ app/etc/config.php
@@ -299,6 +299,9 @@ return [
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
         'WebShopApps_MatrixRate' => 1,
-        'Yotpo_Yotpo' => 0
+        'Yotpo_Yotpo' => 0,
+        'Zero1_Base' => 1,
+        'Zero1_Patches' => 1,
+        'Zero1_ReleaseLibrary' => 1
     ]
 ];
